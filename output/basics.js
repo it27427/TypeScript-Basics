1d57292bfb7f7d8143248ecb5a3782342c6606f1
it27427 <istiak.tushar.qtec@gmail.com>
2022-06-26 22:46:27 +0600

    Type Aliases

--- output/basics.js
+++ output/basics.js
@@ -1,5 +1,13 @@
 "use strict";
-function square(n1, n2) {
+const square = (n1, n2) => {
     return n1 ** n2;
-}
-console.log(square(4, 2));
+};
+const result = square(4, 2);
+console.log(result);
+const userDetails = (id, user) => {
+    console.log(`User id is ${id}, name is ${user.name} and age is ${user.age}`);
+};
+const sayHello = (user) => {
+    console.log(`Hello ${user.age > 40 ? 'Sir' : 'Mr.'} ${user.name}`);
+};
+// Function Signatures
--- src/basics.ts
+++ src/basics.ts
@@ -1,5 +1,21 @@
-function square(n1: number, n2: number) {
+const square:Function = (n1: number, n2: number):number => {
   return n1 ** n2;
 }
 
-console.log(square(4, 2));
+const result = square(4, 2);
+
+console.log(result);
+
+// Type Aliases
+type stringOrNumber = string | number;
+type userType = { name: string, age: number };
+
+const userDetails = (id: stringOrNumber, user: userType) => {
+  console.log(`User id is ${id}, name is ${user.name} and age is ${user.age}`)
+}
+
+const sayHello = (user: userType) => {
+  console.log(`Hello ${user.age > 40 ? 'Sir' : 'Mr.'} ${user.name}`)
+}
+
+// Function Signatures
